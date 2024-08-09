26e751f233c14aeb64d0b8ad23774e77b3de9627
Isabella <80723693+Isabella-Mclean@users.noreply.github.com>
2024-08-09 19:29:37 +0100

    Added analytics

--- chatsupport/app/layout.js
+++ chatsupport/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,8 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <UserProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
       </UserProvider>
+      
     </html>
   );
 }
